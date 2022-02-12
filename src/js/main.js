c9d556a4a257c3c0afdd684da37fd5543517076c
wisheee <wishee.u@gmail.com>
2022-02-12 23:46:53 +0900

    feat: footer 영역 퍼블리싱

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,34 @@
 import { init } from './swiper';
 
 (() => {
+  const container = document.querySelector('.container');
+  const familySiteWrap = document.querySelector('.js-family-site-btn-wrap');
+
+  container.addEventListener('click', e => {
+
+    // 패밀리 사이트 버튼 클릭 이벤트
+    let familySiteBtn = getTarget(e.target, 'js-family-site-btn');
+    if (familySiteBtn && !familySiteWrap.classList.contains('expand')) {
+      familySiteWrap.classList.add('expand');
+    } else {
+      familySiteWrap.classList.remove('expand');
+    }
+    
+  });
+
+  function getTarget(elem, className) {
+    while (!elem.classList.contains(className)) {
+      elem = elem.parentNode;
+
+      if (elem.nodeName == 'BODY') {
+        elem = null;
+        return;
+      }
+    }
+
+    return elem;
+  }
+
   window.addEventListener('DOMContentLoaded', () => {
     document.body.classList.remove('before-load');
   });
